c62457c2de0afc2a5596feeb3985419e434e8a05
sowiks <sowiks@student.mini.pw.edu.pl>
2019-05-26 10:06:43 +0200

    Add function for checking if cycles exists in graph and unit tests

--- color_coding/graph_utils.py
+++ color_coding/graph_utils.py
@@ -5,12 +5,33 @@ def draw_graph():
     G = nx.Graph()
     G.add_edges_from(
         [
-            (1,2), (2,3), (3, 4), (3,1)
+            (0,1),(1,2), (2,3), (3, 4), (3,1)
         ]
     )
     nx.draw(G, node_color=range(4) ,cmap=plt.cm.gist_rainbow)
     plt.show()
     
+def has_any_cycles(graph: nx.Graph) -> bool:
+    n = graph.number_of_nodes()
+    visited = [False] * n
+
+    def traverse_graph(cv, lv):
+        visited[cv] = True
+        # import pdb; pdb.set_trace()
+        for nv in graph.neighbors(cv):
+            if (nv == lv):
+                continue
+            if visited[nv]:
+                return True
+            if traverse_graph(nv, cv):
+                return True
+        return False
+
+    for v in graph.nodes:
+        if not visited[v]:
+            if traverse_graph(v, -1):
+                return True
+    return False
 
 if __name__ == "__main__":
     draw_graph()
---
+++ tests/test_color_coding.py
@@ -0,0 +1,16 @@
+import pytest 
+import networkx as nx
+from color_coding.graph_utils import  has_any_cycles
+
+@pytest.mark.parametrize(
+    "edges_list, expected",
+    [
+        ([(0,1), (1,2), (2,3), (3, 4), (3,1)],True),
+        ([(0,1), (1,2), (2,3), (3, 4), (2,5)],False)
+    ]
+)
+def test_has_any_cycles_finds_cycle(edges_list, expected):
+    g = nx.Graph()
+    g.add_edges_from( edges_list)
+
+    assert expected == has_any_cycles(g)
