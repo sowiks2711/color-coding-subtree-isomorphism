a1aae36805b86c54646b2c28ace940a557b955cc
sowiks <sowiks@student.mini.pw.edu.pl>
2019-05-26 10:47:11 +0200

    Add style guides configuration to vscode and fix style in source code

--- color_coding/graph_utils.py
+++ color_coding/graph_utils.py
@@ -1,25 +1,27 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 def draw_graph():
-    G = nx.Graph()
-    G.add_edges_from(
+    graph = nx.Graph()
+    graph.add_edges_from(
         [
-            (0,1),(1,2), (2,3), (3, 4), (3,1)
+            (0, 1), (1, 2), (2, 3), (3, 4), (3, 1)
         ]
     )
-    nx.draw(G, node_color=range(4) ,cmap=plt.cm.gist_rainbow)
+    nx.draw(graph, node_color=range(4), cmap=plt.cm.gist_rainbow)
     plt.show()
-    
+
+
 def has_any_cycles(graph: nx.Graph) -> bool:
     n = graph.number_of_nodes()
     visited = [False] * n
 
-    def traverse_graph(cv, lv):
+    def traverse_graph(cv: int, lv: int):
         visited[cv] = True
         # import pdb; pdb.set_trace()
         for nv in graph.neighbors(cv):
-            if (nv == lv):
+            if nv == lv:
                 continue
             if visited[nv]:
                 return True
@@ -32,6 +34,7 @@ def has_any_cycles(graph: nx.Graph) -> bool:
             if traverse_graph(v, -1):
                 return True
     return False
+
 
 if __name__ == "__main__":
     draw_graph()
