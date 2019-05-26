71dd4e6c04d3cd4b237db5fbf0939258e723b8e1
sowiks <sowiks@student.mini.pw.edu.pl>
2019-05-26 11:04:55 +0200

    Add setup and Makefile

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sentinel_connectors",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Color coding for subtree isomorphism project for Algorithmics\
+ of computationally difficult problems classes at the Warsaw University\
+ of Technology",
+    author="Tymoteusz Czarnota, Sebastian Sowik",
+    license="MIT",
+)
